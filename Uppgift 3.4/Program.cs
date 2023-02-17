8b22ac27b82c1fa4f74f54a697a5cf5373c5e9a5
Tsnem Musa <tm222qf@student.lnu.se>
2023-02-17 16:32:22 +0100

    Add project files.

---
+++ Uppgift 3.4/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace uppgift4
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hur lång låten är ?");
+            Console.Write("minuter :");
+            int minuter = int.Parse(Console.ReadLine());
+            Console.Write("sekunder :");
+            int sekunder = int.Parse(Console.ReadLine());
+            int lång = (minuter * 60) + sekunder;
+            if (lång >= 165 && lång <= 260)
+            {
+                Console.WriteLine("låten får spelas på radiostationen");
+            }
+            else
+            {
+                Console.WriteLine("låten får inte spelas på radiostationen");
+            }
+        }
+    }
+}
+
+
